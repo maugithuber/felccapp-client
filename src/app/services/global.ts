49486e2bbd26fbbdfecb69d569ad219c456331e8
Carlos Mauricio Peñaranda Lino <ingmauriciopl@gmail.com>
2018-06-03 22:23:31 -0400

    start

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var GLOBAL = {
-    url:'http://localhost:3800/api/'
-	// url: 'https://evening-castle-918s12.herokuapp.com/api/'
+    // url:'http://localhost:3800/api/'
+	url: 'https://felccapp-api.herokuapp.com/'
 
 }
